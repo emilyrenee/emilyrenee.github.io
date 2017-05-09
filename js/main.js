a38bb0dddac7406f86b079c7335937e62f0865f3
emily renee <hagoodem@gmail.com>
2017-05-08 21:21:44 -0400

    add hamburger, style logo and add media queries

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+//hamburger nav functionality from CodePen
+$(document).ready(function () {
+			  $(".navbar-toggle").on("click", function () {
+				    $(this).toggleClass("active");
+			  });
+});
+
