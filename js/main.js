216fd72e9e37959d677913fc8f1b0492222bce6a
emily renee <hagoodem@gmail.com>
2017-05-10 17:01:36 -0400

    style hamburger, toggle functionality on mobile

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ $(document).ready(function () {
 			  });
 });
 
+
+$('.btn-group').sticky();
+
+//if dipslay = hidden for panels then toggle class by clicking the chevron
+
+$('.glyphicon-menu-down').on('click', function () {
+	console.log("Hello World");
+	$(this).toggleClass('glyphicon-menu-up');
+	let panelBody = $('.panel-body');
+	if (panelBody.style[display="none"]) {
+		panelBody.css('display', 'inline');
+	}
+});
