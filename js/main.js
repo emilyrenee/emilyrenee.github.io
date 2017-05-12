076a1f7e338cb2e944af8dc87a49e1e3c8140d67
emily renee <hagoodem@gmail.com>
2017-05-12 15:04:10 -0400

    add toggled images for hover on portfolio links

--- js/main.js
+++ js/main.js
@@ -7,6 +7,25 @@ $(document).ready(function () {
 });
 
 
+const portLink = $('.port-link');
+$.each(portLink, function() {
+	$(this).hover(
+  	function() {
+    	$(this).next().css('display', 'block'); 
+    },
+	function() {
+    	$(this).next().css('display', 'none'); 
+    });
+})
+
+
+//rotates chevron and hide/shows details
+$('.panel-title').on('click', function (e) {
+	$(this).children(":first").toggleClass('glyphicon-menu-up');
+	$(this).next().toggleClass('panel-body-show');
+});
+
+//sticky.js plugin responsive sizing
 $('.btn-group').sticky({
   topSpacing: 0,
   //  to scale
@@ -14,8 +33,40 @@ $('.btn-group').sticky({
   responsiveWidth: true
 });
 
-//rotates chevron and hide/shows details
-$('.panel-title').on('click', function (e) {
-	$(this).children(":first").toggleClass('glyphicon-menu-up');
-	$(this).next().toggleClass('panel-body-show');
+
+
+const logoContent = $('.logo').html();
+const logoContentInitials = "<h1>EH</h1>";
+
+$(document).ready(function(){
+	let windowSize = $(window).width();
+	if (windowSize >= 768){	
+		console.log('hello world');
+		$('.unstuck-logo').sticky({
+		  topSpacing: 0,
+		  //  to scale
+		  getWidthFrom: '.container',
+		  responsiveWidth: true
+		});
+
+		$('.panel').css('position', 'relative');
+		$('.panel').css('z-index', '-1');
+		$('.logo-link').css('z-index', '5000'); //not working?
+
+		$('.unstuck-logo').on('sticky-start', function () {
+			$(this).html(logoContentInitials).addClass('logo-sticker');
+			$(this).css("z-index", "350");
+		});
+
+		$('.unstuck-logo').on('sticky-end', function () {
+			$(this).append(logoContent).addClass('unstuck-logo');
+			$(this).removeClass('logo-sticker');
+			$(this).filter('div:contains("EH"):first-child').remove();
+		});
+	}	
 });
+
+
+
+
+
